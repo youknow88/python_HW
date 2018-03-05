7e62f90bed31922e3a006b9ab00a682674ce4485
Yevgeny <genya.v88@gmail.com>
2018-03-05 16:50:27 +0300

    hw7 fixed

--- hw7.py
+++ hw7.py
@@ -1,7 +1,7 @@
 
 american_date = '06.17.2016'
-month = (american_date[:2])
-day = (american_date[3:5])
-year = (american_date[6:])
-european_date = str(day) + str(month) + str(year)
+month = american_date[:2]
+day = american_date[3:5]
+year = american_date[6:]
+european_date = day + month + year
 print(european_date)

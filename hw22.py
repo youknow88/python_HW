913fd5c9dd08b615088e55f8641109ad78c78820
Yevgeny <genya.v88@gmail.com>
2018-03-22 16:16:28 +0300

    hw8

--- hw22.py
+++ hw22.py
@@ -1,18 +1,52 @@
 import random
 
+# def greeting():
+#     print('-------------------------')
+#     print('Please enter your number:')
+#     print('-------------------------')
+#
+# def guess_game():
+#         greeting()
+#         program_choice = random.randint(1, 10)
+#         while user_choice != program_choice:
+#             user_choice = int(input('>'))
+#             if user_choice > program_choice:
+#                 print('Less')
+#             if user_choice < program_choice:
+#                 print('More')
+#         print('You Guess')
+# guess_game()
+
+import random
+
+
 def greeting():
     print('-------------------------')
+
     print('Please enter your number:')
+
     print('-------------------------')
 
+
 def guess_game():
-        greeting()
-        program_choice = random.randint(1, 10)
-        while user_choice != program_choice:
-            user_choice = int(input('>'))
-            if user_choice > program_choice:
-                print('Less')
-            if user_choice < program_choice:
-                print('More')
-        print('You Guess')
+    greeting()
+
+    program_choice = random.randint(1, 10)
+
+    while True:
+
+        user_choice = int(input('>'))
+
+        if user_choice == program_choice:
+            print('You Guess')
+
+            break
+
+        if user_choice > program_choice:
+            print('Less')
+
+        if user_choice < program_choice:
+            print('More')
+
+
 guess_game()

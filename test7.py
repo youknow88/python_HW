b649435dc4f93858f3b0a94efc01d9472b124aa7
Yevgeny <genya.v88@gmail.com>
2018-04-08 10:55:47 +0300

    test7_fixed

--- test7.py
+++ test7.py
@@ -1,11 +1,11 @@
 
 def fibonacci_numbers(n):
-    a = 1
-    b = 2
-    for i in range(n):
+    fibonacci_list = [1]
+    a, b = 0, 1
+    for i in range(2, n + 1):
         a, b = b, a + b
-    return a
+        fibonacci_list.append(b)
+    return fibonacci_list
 
-print(fibonacci_numbers(10))
 
-
+print(sum(fibonacci_numbers(10)))
